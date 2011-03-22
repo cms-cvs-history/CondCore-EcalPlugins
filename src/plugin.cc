11802325247a655006861e3fb22768b5605145cb
cmsbuild <>
2011-03-22 16:14:07 +0000

    This commit was manufactured by cvs2git to create tag 'CondCore- EcalPlugins-V01-05-01'.
    Sprout from master 2010-10-25 22:31:22 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Convert BuildFiles to XML'
    Cherrypick from master 2011-03-22 16:13:03 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'added EcalTimeOffset record':
        src/plugin.cc

--- src/plugin.cc
+++ src/plugin.cc
@@ -43,6 +43,8 @@
 #include "CondFormats/DataRecord/interface/EcalTimeCalibConstantsRcd.h"
 #include "CondFormats/EcalObjects/interface/EcalTimeCalibErrors.h"
 #include "CondFormats/DataRecord/interface/EcalTimeCalibErrorsRcd.h"
+#include "CondFormats/EcalObjects/interface/EcalTimeOffsetConstant.h"
+#include "CondFormats/DataRecord/interface/EcalTimeOffsetConstantRcd.h"
 
 #include "CondFormats/EcalObjects/interface/EcalADCToGeVConstant.h"
 #include "CondFormats/DataRecord/interface/EcalADCToGeVConstantRcd.h"
@@ -109,6 +111,12 @@
 #include "CondFormats/EcalObjects/interface/EcalTPGTowerStatus.h"
 #include "CondFormats/DataRecord/interface/EcalTPGTowerStatusRcd.h"
 
+#include "CondFormats/EcalObjects/interface/EcalTPGStripStatus.h"
+#include "CondFormats/DataRecord/interface/EcalTPGStripStatusRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalTPGSpike.h"
+#include "CondFormats/DataRecord/interface/EcalTPGSpikeRcd.h"
+
 #include "CondFormats/EcalObjects/interface/EcalDCSTowerStatus.h"
 #include "CondFormats/DataRecord/interface/EcalDCSTowerStatusRcd.h"
 
@@ -132,6 +140,7 @@ REGISTER_PLUGIN(EcalIntercalibConstantsRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalIntercalibConstantsMCRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalTimeCalibConstantsRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalTimeCalibErrorsRcd,EcalFloatCondObjectContainer);
+REGISTER_PLUGIN(EcalTimeOffsetConstantRcd,EcalTimeOffsetConstant);
 REGISTER_PLUGIN(EcalIntercalibErrorsRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalADCToGeVConstantRcd,EcalADCToGeVConstant);
 REGISTER_PLUGIN(EcalLaserAlphasRcd,EcalFloatCondObjectContainer);
@@ -160,6 +169,8 @@ REGISTER_PLUGIN(EcalTPGWeightGroupRcd,EcalTPGWeightGroup);
 REGISTER_PLUGIN(EcalTPGWeightIdMapRcd,EcalTPGWeightIdMap);
 REGISTER_PLUGIN(EcalTPGCrystalStatusRcd,EcalTPGCrystalStatus);
 REGISTER_PLUGIN(EcalTPGTowerStatusRcd,EcalTPGTowerStatus);
+REGISTER_PLUGIN(EcalTPGStripStatusRcd,EcalTPGStripStatus);
+REGISTER_PLUGIN(EcalTPGSpikeRcd,EcalTPGSpike);
 
 REGISTER_PLUGIN(EcalDCSTowerStatusRcd,EcalDCSTowerStatus);
 REGISTER_PLUGIN(EcalDAQTowerStatusRcd,EcalDAQTowerStatus);
